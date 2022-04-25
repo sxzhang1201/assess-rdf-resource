c54d65118d107c129b164e9a7da8b1af51e20bb4
Shuxin Zhang <s.x.zhang1201@gmail.com>
2022-04-25 10:24:43 +0200

    Workflow Scripts

--- run_assessment.py
+++ run_assessment.py
@@ -1,29 +1,36 @@
 import time
+from rdflib.term import URIRef
 
+# Import metrics
 from metrics.coverage import count_rdf_component as count_uri
 from metrics.resolvability import get_http_status_code as assess_resolvability
 from metrics.parsability import assess_parsable
 from metrics.classify import extract_type as classify_uri
 from metrics.consistency import run_consistency as assess_consistency
 
+# Import basic functions
 from functions.load_resource import parse_data_local_or_remote as load_resource
 from functions.assessment_report import AssessReport
+from functions.save_or_load_report import save_object
+from functions.inspect_items import view_graph
 
-from rdflib.term import URIRef
-
+# Import human-configurable
+from config import WhichResource, WhichReport, LABEL
 
 # Calculate Time Cost
 print("Start assessment!")
 start_time = time.time()
 
 # Load data
-g = load_resource('resources/A multicenter registry for nasopharyngeal cancer in children copy.ttl')
+path = WhichResource[LABEL]
+g = load_resource(path)
 
-for s, p, o in g:
-    print(s, p, o)
+# Quick view of first {10} triples
+view_graph(g, 10)
 
 # Run coverage and get basic characteristics
 uri_list = count_uri(g=g, rdf_component_type=URIRef)
+print("0. There are {} URIs in the test RDF graph".format(len(uri_list)))
 
 print("1. Test Resolvability ")
 # Run resolvable test
@@ -39,12 +46,15 @@ print("2.5 Classify URIs ")
 class_list, property_list, owl_datatype_property, owl_object_property, other_property_list, others_list = \
     classify_uri(parsable_uri)
 
+# Run consistency
+print("3. Test Consistency")
 misplaced_class_list, misplaced_property_list, misused_datatype_property_list, misused_object_property_list = \
     assess_consistency(g=g, class_list=class_list,
                        property_list=property_list,
                        datatype_property_list=owl_datatype_property,
                        object_property_list=owl_object_property)
 
+print("4. Generate Report")
 # Populate reports
 r = AssessReport()
 
@@ -62,28 +72,23 @@ r.add_misplaced_property(misplaced_property_list)
 r.add_misused_owl_datatype_property(misused_datatype_property_list)
 r.add_misused_owl_object_property(misused_object_property_list)
 
-print(r.uri_list)
-
-# Populate Resolvability
+# Populate Resolvability test result
 r.add_non_res_uri(non_res_uri_list)
 
-# Populate Parsability
+# Populate Parsability test result
 r.add_parsability_errors(error1_rdf_no_triple)
 
-# Populate Consistency
+# Populate Consistency test result
 r.add_undefined_uri(error2_uri_undefined)
 
+# Save Report
+save_object(obj=r, filename=WhichReport[LABEL])
 
-print(len(r.resolvability_errors))
-print(r.resolvability_errors)
-print(len(r.uri_list))
-
-# Calculate affected triples
+# Calculate affected triples of non-resolvable URIs
 non_res_rate = r.resolvability_statistics()
-print(non_res_rate)
+print('The proportion of affected triples is {}.'.format(non_res_rate))
 
 # End Time
 print("\nThe Processing Time is {}min".format(
     format((time.time() - start_time) / 60, ".2f")))
-
 
