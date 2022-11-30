9f9e7ee5c1694e84e6bd573474d3447157edbfb9
Shuxin Zhang <s.x.zhang1201@gmail.com>
2022-11-30 17:03:38 +0100

    Update load_resource.py
    Enable automatic selection of parse type

--- functions/load_resource.py
+++ functions/load_resource.py
@@ -1,12 +1,19 @@
 import os.path
 from rdflib.graph import Graph
+import requests
+import corpus
 
 
 def parse_data_local_or_remote(data_path):
     g = Graph()
     if 'http' in str(data_path):
         # print(" Parse Remote Graph!")
-        g.parse(data_path)
+        r = requests.get(data_path, headers=corpus.Headers)
+        content_type_this_uri = r.headers["content-type"].split(";", 1)[0]
+        print(content_type_this_uri)
+        parse_format = corpus.MapContentTypeToParserFormat[content_type_this_uri]
+        g.parse(data_path, format=parse_format)
+
     else:
         # print(" Parse Local Graph!")
         # Check 1) if the file exists and 2) if RDF graphs in that file is parsable
